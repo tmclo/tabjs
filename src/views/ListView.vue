b37baf81b44c1705e3bc0dbc0b839d4793b23b84
Tom McLoughlin <thomas@springsy.co.uk>
2022-05-22 13:57:59 +0100

    add tabs list

---
+++ src/views/ListView.vue
@@ -0,0 +1,20 @@
+<template>
+
+  <main>
+    <div class="list p-10 flex flex-wrap justify-center gap-8">
+        <ListTabs />
+    </div>
+  </main>
+
+</template>
+
+<script>
+import ListTabs from '../components/ListTabs.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    ListTabs
+  }
+}
+</script>
